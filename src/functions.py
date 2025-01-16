bd377fed4b8865c0ba71892cd7b2f6ab8283eae0
Santiago-Farias <santifa8@gmail.com>
2025-01-15 23:36:30 -0300

    Initial commit

---
+++ src/functions.py
@@ -0,0 +1,84 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import os
+import time
+
+def take_screenshot(element, filename):
+    element.screenshot(filename)
+
+def int_input_check(input_message: str):
+    int_input = input(input_message)
+    if int_input.isdigit() != True:
+        print("¡Invalid entry!")
+        int_input = input(input_message)
+    else:
+        int_input = int(int_input)
+        return int_input
+
+def get_fullscreen_screenshot(output_folder: str, driver):
+    fullscreen_screenshot_path = os.path.join(output_folder, f'caca.png')
+    fullscreen_shot = driver.get_screenshot_as_file(fullscreen_screenshot_path)
+
+def get_element_screenshot_cyclic(cycles: int, element_capture, output_folder: str, driver, secs_interval: int):
+    for i in range(cycles):
+        element = element_capture # content
+        screenshot_path = os.path.join(output_folder, f'screenshot_{i+1}.png')
+        take_screenshot(element, screenshot_path)
+
+        next_button = driver.find_element(By.ID, 'next') # next
+        next_button.click()
+        time.sleep(secs_interval)
+
+def get_element_screenshot(element_capture, output_folder: str, driver):
+    element = element_capture # content
+    screenshot_path = os.path.join(output_folder, f'screenshot_{1}.png')
+    take_screenshot(element, screenshot_path)
+
+def element_to_capture(elements: list, driver):
+    element_type_name = 0
+    geraut = False
+    element_type = input("Enter the type of element to capture: ")
+    while True:
+        for i in range(0, len(elements)):
+            if element_type == elements[i]:
+                geraut = True
+                break
+        if geraut == True:
+            break
+        if i == len(elements) and geraut == False:
+            element_type_name = f"{element_type} is a non-existent element."
+        break
+
+    if type(element_type_name) != str:
+        element_name = input(f"Enter the name of ({element_type}) element: ")
+
+        element_type_name = ""
+        if element_type.lower() == 'id':
+            try:
+                element_type_name = driver.find_element(By.ID, element_name) # type: ignore
+            except:
+                element_type_name = f"ERROR: the name {element_name} of element {element_type} does not exist"
+        elif element_type.lower() == 'class':
+            try:
+                element_type_name = driver.find_element(By.CLASS_NAME, element_name) # type: ignore
+            except:
+                element_type_name = f"ERROR: the name {element_name} of element {element_type} does not exist"
+        elif element_type.lower() == 'name':
+            try:
+                element_type_name = driver.find_element(By.NAME, element_name) # type: ignore
+            except:
+                element_type_name = f"ERROR: the name {element_name} of element {element_type} does not exist"
+        elif element_type.lower() == 'tag name':
+            try:
+                element_type_name = driver.find_element(By.TAG_NAME, element_name) # type: ignore
+            except:
+                element_type_name = f"ERROR: the name {element_name} of element {element_type} does not exist"
+        elif element_type.lower() == 'link text':
+            try:
+                element_type_name = driver.find_element(By.LINK_TEXT, element_name) # type: ignore
+            except:
+                element_type_name = f"ERROR: the name {element_name} of element {element_type} does not exist"
+    else:
+        element_type_name = f"'{element_type}' is a non-existent element in URL."
+    return element_type_name
---
+++ src/main.py
@@ -0,0 +1,75 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+import time
+from PIL import Image
+import os
+from functions import *
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--log-level=1")
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.google.com/")
+
+service = Service('D:\Programacion\Recursos\chromedriver-win64\chromedriver.exe')
+driver = webdriver.Chrome(service=service)
+driver.get('file:///D:/Programacion/Proyectos/screenshots_saver/index.html')
+elements = ['id', 'class', 'name', 'tag name', 'link text']
+menu_option_input = 0
+cycles = int
+output_folder = 'screenshots'
+
+while True:
+    print("1. Capture an element and continue capturing after clicking")
+    print("2. Capture an element")
+    print("3. Capture a full screen of current screen")
+    print("4. Exit")
+
+    menu_option_input = int_input_check("Enter a option: ")
+
+    if menu_option_input == 1:
+        element_capture = element_to_capture(elements, driver)
+        if type(element_capture) != str:
+            cycles = int_input_check("Enter the number of captures do you want to do of a element: ")
+            if cycles > 1:
+                secs_interval = int_input_check("Enter the seconds interval between captures: ")
+            else:
+                secs_interval = 0
+            print("Capturing elemnt...")
+            get_element_screenshot_cyclic(cycles, element_capture, output_folder, driver, secs_interval)
+            print("¡Element captured!")
+        else:
+            print(element_capture)
+    elif menu_option_input == 2:
+        element_capture = element_to_capture(elements, driver)
+        if type(element_capture) != str:
+            print("Capturing elemnt...")
+            get_element_screenshot(element_capture, output_folder, driver)
+            print("¡Element captured!")
+        else:
+            print(element_capture)
+    elif menu_option_input == 3:
+        print("Capturing the full screen...")
+        get_fullscreen_screenshot(output_folder, driver)
+        print("¡Screen captured!")
+    elif menu_option_input == 4:
+        print("Leaving please wait..")
+        driver.quit()
+        break
+    else:
+        print("Numeber of option doesn't exist")
+
+driver.quit()
+
+"""
+Agregar:
+- Captura de pantalla completa CHECK
+- La posibilidad de sacar una foto, sin cliks. CHECK
+- Revisar en get_element_screenshot() que si el elemento no existe, diga que no exite y que no se rompa. CHECK
+- Revisar la funcion get_element_screenshot_cyclic() para hacer las validaciones correspondientes y que no rompa.
+- Hacer una gesión de carpetas, donde se guarden las capturas según el tipo de carpeta, las mismas deben tener sub-carpetas con la fecha. Si ya existe la carpeta con fecha
+hacer que no rompa.
+- Hacer un sistema donde te pida la url y luego el menú, tantas veces como quiera el usuario hasta que diga BASTA.
+"""
